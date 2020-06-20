554a1262c50bc8921255ed767820eb47ec9b5ca4
“Kaustavi <kaustavisen8@gmail.com>
2020-06-20 23:55:52 +0530

    Refactored:Merge all sorting functions using generic method

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -1,33 +1,13 @@
 package com.bridgelabz;
 
 public class FindMaximum {
-    public Integer maximum(Integer firstNumber, Integer secondNumber, Integer ThirdNumber) {
-        Integer maximum = firstNumber;
-        if (secondNumber.compareTo(maximum) > 0) {
-            maximum = secondNumber;
+    public static <T extends Comparable<T>> T maximumElement(T first,T second,T third) {
+        T maximum = first;
+        if (second.compareTo(maximum) > 0) {
+            maximum = second;
         }
-        if (ThirdNumber.compareTo(maximum) > 0) {
-            maximum = ThirdNumber;
-        }
-        return maximum;
-    }
-    public Double maximumOfFloat(Double firstNumber, Double secondNumber, Double ThirdNumber) {
-        Double maximum = firstNumber;
-        if (secondNumber.compareTo(maximum) > 0) {
-            maximum = secondNumber;
-        }
-        if (ThirdNumber.compareTo(maximum) > 0) {
-            maximum = ThirdNumber;
-        }
-        return maximum;
-    }
-    public String maximumOfString(String firstString, String secondString, String ThirdString) {
-        String maximum = firstString;
-        if (secondString.compareTo(maximum) > 0) {
-            maximum = secondString;
-        }
-        if (ThirdString.compareTo(maximum) > 0) {
-            maximum = ThirdString;
+        if (third.compareTo(maximum) > 0) {
+            maximum = third;
         }
         return maximum;
     }
--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -6,39 +6,39 @@ public class FindMaximumTest {
     FindMaximum findMaximum = new FindMaximum();
     @Test
     public void maxNumberAtFirstPosition() {
-        Assert.assertEquals(45,(int)findMaximum.maximum(45, 30,20));
+        Assert.assertEquals(45,(int)findMaximum.maximumElement(45, 30,20));
     }
     @Test
     public void maxNumberAtSecondPosition() {
-        Assert.assertEquals(100,(int)findMaximum.maximum(90, 100,73));
+        Assert.assertEquals(100,(int)findMaximum.maximumElement(90, 100,73));
     }
     @Test
     public void maxNumberAtThirdPosition() {
-        Assert.assertEquals(76,(int)findMaximum.maximum(45, 30,76));
+        Assert.assertEquals(76,(int)findMaximum.maximumElement(45, 30,76));
     }
     @Test
     public void maxFloatAtFirstPosition() {
-        Assert.assertEquals((Double) 23.9,(Double)findMaximum.maximumOfFloat(23.9, 3.9,2.5));
+        Assert.assertEquals((Double) 23.9,(Double)findMaximum.maximumElement(23.9, 3.9,2.5));
     }
     @Test
     public void maxFloatAtSecondPosition() {
-        Assert.assertEquals((Double)8.4,(Double)findMaximum.maximumOfFloat(6.7, 8.4,2.5));
+        Assert.assertEquals((Double)8.4,(Double)findMaximum.maximumElement(6.7, 8.4,2.5));
     }
     @Test
     public void maxFloatAtThirdPosition() {
-        Assert.assertEquals((Double)98.2,(Double)findMaximum.maximumOfFloat(0.9, 45.9,98.2));
+        Assert.assertEquals((Double)98.2,(Double)findMaximum.maximumElement(0.9, 45.9,98.2));
     }
     @Test
     public void maxStringAtFirstPosition() {
-        Assert.assertEquals((String) "Grape",(String)findMaximum.maximumOfString("Grape", "Banana","Apple"));
+        Assert.assertEquals((String) "Grape",(String)findMaximum.maximumElement("Grape", "Banana","Apple"));
     }
     @Test
     public void maxStringAtSecondPosition() {
-        Assert.assertEquals((String)"panda",(String)findMaximum.maximumOfString("dog", "panda","cat"));
+        Assert.assertEquals((String)"panda",(String)findMaximum.maximumElement("dog", "panda","cat"));
     }
     @Test
     public void maxStringAtThirdPosition() {
-        Assert.assertEquals((String) "pune",(String)findMaximum.maximumOfString("bangalore", "kolkata","pune"));
+        Assert.assertEquals((String) "pune",(String)findMaximum.maximumElement("bangalore", "kolkata","pune"));
     }
 
 }
