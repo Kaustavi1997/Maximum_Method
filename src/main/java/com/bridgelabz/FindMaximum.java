f585e4f758a5b1a3eaa2dd1e876a1c4b8b3b9389
“Kaustavi <kaustavisen8@gmail.com>
2020-06-21 01:16:39 +0530

    Added:used generic collection to find maximum from a list

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -1,25 +1,9 @@
 package com.bridgelabz;
+import java.util.Arrays;
 
-public class FindMaximum<T extends Comparable<T>>{
-    T first,second,third;
-    public FindMaximum(T first,T second,T third){
-        this.first=first;
-        this.second=second;
-        this.third=third;
-    }
-
-    public T maximum(){
-        return FindMaximum.maximumElement(first,second,third);
-    }
-
-    public static <T extends Comparable<T>> T maximumElement(T first,T second,T third) {
-        T maximum = first;
-        if (second.compareTo(maximum) > 0) {
-            maximum = second;
-        }
-        if (third.compareTo(maximum) > 0) {
-            maximum = third;
-        }
-        return maximum;
+public class FindMaximum{
+    public static <T extends Comparable<T>> T getMaximum(T[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length-1];
     }
 }
--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -3,44 +3,22 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class FindMaximumTest {
+    FindMaximum findMaximum = new FindMaximum();
     @Test
-    public void maxNumberAtFirstPosition() {
-        Assert.assertEquals(45,new FindMaximum(45, 30,20).maximum());
+    public void maxInt() {
+        Integer[] arr = {13, 7, 6, 45, 21, 9, 2, 100};
+        Assert.assertEquals(100,(int)findMaximum.getMaximum(arr));
     }
 
     @Test
-    public void maxNumberAtSecondPosition() {
-        Assert.assertEquals(100,new FindMaximum(90, 100,73).maximum());
-    }
-    @Test
-    public void maxNumberAtThirdPosition() {
-        Assert.assertEquals(76,new FindMaximum(45, 30,76).maximum());
-    }
-    @Test
-    public void maxFloatAtFirstPosition() {
-        Assert.assertEquals(23.9,new FindMaximum(23.9, 3.9,2.5).maximum());
+    public void maxDouble() {
+        Double[] arr = {2.62,1.32,5.987,4.43};
+        Assert.assertEquals((Double)5.987,(Double)findMaximum.getMaximum(arr));
     }
 
     @Test
-    public void maxFloatAtSecondPosition() {
-        Assert.assertEquals(8.4,new FindMaximum(6.7, 8.4,2.5).maximum());
+    public void maxString() {
+        String[] arr = {"kolkata","bengaluru","delhi","pune","mumbai"};
+        Assert.assertEquals((String) "pune",(String) findMaximum.getMaximum(arr));
     }
-    @Test
-    public void maxFloatAtThirdPosition() {
-        Assert.assertEquals(98.2,new FindMaximum(0.9, 45.9,98.2).maximum());
-    }
-    @Test
-    public void maxStringAtFirstPosition() {
-        Assert.assertEquals("Grape",new FindMaximum("Grape", "Banana","Apple").maximum());
-    }
-
-    @Test
-    public void maxStringAtSecondPosition() {
-        Assert.assertEquals("panda",new FindMaximum("dog", "panda","cat").maximum());
-    }
-    @Test
-    public void maxStringAtThirdPosition() {
-        Assert.assertEquals("pune",new FindMaximum("bangalore", "kolkata","pune").maximum());
-    }
-
 }
