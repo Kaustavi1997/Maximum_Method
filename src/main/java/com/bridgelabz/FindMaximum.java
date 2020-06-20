4562d1492dd3388d555730fe2a9e0e12921f759b
“Kaustavi <kaustavisen8@gmail.com>
2020-06-21 01:47:20 +0530

    Added:print the max elements using printMax generic method

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -1,9 +1,17 @@
 package com.bridgelabz;
 import java.util.Arrays;
 
-public class FindMaximum{
-    public static <T extends Comparable<T>> T getMaximum(T[] arr) {
+public class FindMaximum<T extends Comparable<T>>{
+    T[] arr;
+    public FindMaximum(T[] arr){
+        this.arr = arr;
+    }
+    public T getMaximum() {
         Arrays.sort(arr);
         return arr[arr.length-1];
+    }
+
+    public void printMaximum(){
+        System.out.println(arr[arr.length-1]);
     }
 }
--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -3,22 +3,28 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class FindMaximumTest {
-    FindMaximum findMaximum = new FindMaximum();
+
     @Test
     public void maxInt() {
-        Integer[] arr = {13, 7, 6, 45, 21, 9, 2, 100};
-        Assert.assertEquals(100,(int)findMaximum.getMaximum(arr));
+        Integer[] arr = {13, 7, 6, 45, 21, 9, 200, 100};
+        FindMaximum findMaximum = new FindMaximum(arr);
+        Assert.assertEquals(200,(int)findMaximum.getMaximum());
+        findMaximum.printMaximum();
     }
 
     @Test
     public void maxDouble() {
         Double[] arr = {2.62,1.32,5.987,4.43};
-        Assert.assertEquals((Double)5.987,(Double)findMaximum.getMaximum(arr));
+        FindMaximum findMaximum = new FindMaximum(arr);
+        Assert.assertEquals((Double)5.987,(Double)findMaximum.getMaximum());
+        findMaximum.printMaximum();
     }
 
     @Test
     public void maxString() {
         String[] arr = {"kolkata","bengaluru","delhi","pune","mumbai"};
-        Assert.assertEquals((String) "pune",(String) findMaximum.getMaximum(arr));
+        FindMaximum findMaximum = new FindMaximum(arr);
+        Assert.assertEquals((String) "pune",(String) findMaximum.getMaximum());
+        findMaximum.printMaximum();
     }
 }
