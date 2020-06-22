1010fc1d041f8fefa5c4e43bd1ba9a42e4b72b7d
“Kaustavi <kaustavisen8@gmail.com>
2020-06-22 21:03:08 +0530

    Refactored:removed unnecessary codes in FindMaximum

--- src/main/java/com/bridgelabz/FindMaximum.java
+++ src/main/java/com/bridgelabz/FindMaximum.java
@@ -2,21 +2,12 @@ package com.bridgelabz;
 import java.util.Arrays;
 
 public class FindMaximum<T extends Comparable<T>>{
-    private final T[] arguments;
-    public FindMaximum(T... arguments){
-        this.arguments = arguments;
-    }
-    public T getMaximum(){
-        return FindMaximum.getMaximum(arguments);
-    }
     public static <T> T getMaximum(T... arguments) {
-        T maximumValue;
         Arrays.sort(arguments);
-        maximumValue = arguments[arguments.length-1];
+        T maximumValue = arguments[arguments.length-1];
         printMaximum(maximumValue);
         return maximumValue;
     }
-
     public static <T> void printMaximum(T maximumValue){
         System.out.println("Maximum value:"+maximumValue);
     }
--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -3,22 +3,18 @@ import org.junit.Test;
 import org.junit.Assert;
 
 public class FindMaximumTest {
-
     @Test
     public void maxInt() {
-        FindMaximum findMaximum = new FindMaximum(13, 7, 6, 45, 21, 9, 200, 100);
-        Assert.assertEquals(200,findMaximum.getMaximum());
+        Assert.assertEquals((Integer)80,FindMaximum.getMaximum(2,5,80,69,13));
     }
 
     @Test
     public void maxDouble() {
-        FindMaximum findMaximum = new FindMaximum(2.62,1.32,5.987,4.43);
-        Assert.assertEquals(5.987,findMaximum.getMaximum());
+        Assert.assertEquals((Double)5.987,FindMaximum.getMaximum(2.62,1.32,5.987,4.43));
     }
 
     @Test
     public void maxString() {
-        FindMaximum findMaximum = new FindMaximum("kolkata","bengaluru","delhi","pune","mumbai");
-        Assert.assertEquals("pune",findMaximum.getMaximum());
+        Assert.assertEquals("pune",FindMaximum.getMaximum("kolkata","bengaluru","delhi","pune","mumbai"));
     }
 }
