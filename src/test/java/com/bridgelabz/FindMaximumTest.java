6f002d47b5bbe6d008f376fdcf3bb0520ec7630b
“Kaustavi <kaustavisen8@gmail.com>
2020-06-22 17:07:10 +0530

    Refactored:Removed Type casting

--- src/test/java/com/bridgelabz/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/FindMaximumTest.java
@@ -7,18 +7,18 @@ public class FindMaximumTest {
     @Test
     public void maxInt() {
         FindMaximum findMaximum = new FindMaximum(13, 7, 6, 45, 21, 9, 200, 100);
-        Assert.assertEquals(200,(int)findMaximum.getMaximum());
+        Assert.assertEquals(200,findMaximum.getMaximum());
     }
 
     @Test
     public void maxDouble() {
         FindMaximum findMaximum = new FindMaximum(2.62,1.32,5.987,4.43);
-        Assert.assertEquals((Double)5.987,(Double)findMaximum.getMaximum());
+        Assert.assertEquals(5.987,findMaximum.getMaximum());
     }
 
     @Test
     public void maxString() {
         FindMaximum findMaximum = new FindMaximum("kolkata","bengaluru","delhi","pune","mumbai");
-        Assert.assertEquals((String) "pune",(String) findMaximum.getMaximum());
+        Assert.assertEquals("pune",findMaximum.getMaximum());
     }
 }
